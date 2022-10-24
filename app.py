95cba8e56dd27d59ca9102355d58c9ed6f3a1d8c
danish1790 <aumer4244@gmail.com>
2022-10-24 15:01:47 -0700

    prediction completed using flask

--- app.py
+++ app.py
@@ -18,12 +18,67 @@ def home():
 def predict_api():
     data = request.json['data']
     print(data)
+    if(data.get('Sex')=='Male'):
+        data.update({'Sex':1})
+    else:
+        data.update({'Sex':0})
+    
+
+    a = ['B','C','D','A','F','E','T','Z','G']
+    b = [1,2,3,4,5,6,7,8,9]
+
+    for i in range(len(a)):
+            if(data.get("Block")==a[i]):
+                print('matched')
+                data.update({"Block":b[i]})
+                break
+            else:
+                pass
+            
     print(np.array(list(data.values())).reshape(1,-1))
     new_data = scaler.fit_transform(np.array(list(data.values())).reshape(1,-1))
     output = xgb_model.predict(new_data)
     print(output[0])
 
     return jsonify(int(output[0]))
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    # data=[float(x) for x in request.form.values()]
+    data = list(request.form.values())
+
+    for i in range(len(data)):
+        if 'Male' in data:
+            index = data.index('Male')
+            data[index] = 1
+        if 'Female' in data:
+            index = data.index('Female')
+            data[index] = 0
+    
+
+    a = ['B','C','D','A','F','E','T','Z','G']
+    b = [1,2,3,4,5,6,7,8,9]
+
+
+  
+    for i in range(len(a)):
+            if a[i] in data:
+                index = data.index(a[i])
+                data[index]=b[i]
+                print('matched')
+                break
+            else:
+                pass
+    
+    print(data)
+    final_input = scaler.fit_transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = xgb_model.predict(final_input)[0]
+    # survival = 'Survived' if output==1 else 'Not survived'
+    return render_template('home.html',prediction_no='The prediction of survival is {}'.format(output))
+    # return data
 
 if __name__=="__main__":
     app.run(debug=True)
